e0aa3a0dc1cadcd2ab1e055af010dd6a6a6a2543
dakshhh <69110996+dakshhh@users.noreply.github.com>
2020-08-03 18:44:30 +0530

    Add files via upload

---
+++ MARKS EVALUATION.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+main()
+{
+	char name[40];
+	printf("Enter your name: \n");
+	scanf("%s", &name);
+int marks;
+	printf("Enter Your Marks:");
+	scanf("%d", &marks);
+if(marks>=85 && marks<100)	
+{
+	printf("Grade A");
+}
+else if(marks>=70 && marks<84)
+{
+	printf("Grade B");
+}
+else if(marks>=55 && marks<69)
+{
+	printf("Grade C");
+}
+else if(marks>=40 && marks<54)
+{
+	printf("Grade D");
+}
+else
+{
+	printf("Grade F");
+}
+} 
